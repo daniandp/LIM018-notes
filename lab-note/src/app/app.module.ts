639fe86c8a1f6844ff045bad65e53d40505440a1
daniandp <andradegsp@gmail.com>
2022-10-30 21:10:50 -0500

    Se maqueta vista de registro

--- lab-note/src/app/app.module.ts
+++ lab-note/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
